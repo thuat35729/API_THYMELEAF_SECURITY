e648074ee9ab45025e59f17b1d83229ec83a412f
thuat35729 <149344062+thuat35729@users.noreply.github.com>
2024-07-12 17:41:37 +0700

    p
    s

---
+++ Advanced_Thymeleaf/src/main/java/com/example/advanced_thymeleaf/AdvancedThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.advanced_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedThymeleafApplication.class, args);
+    }
+
+}
---
+++ Advanced_Thymeleaf/src/test/java/com/example/advanced_thymeleaf/AdvancedThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advanced_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
